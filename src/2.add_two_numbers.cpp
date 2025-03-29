4a0d89125df29dec5bb2046289f95b1b57e03a45
knifemaster <emilasanbekov@gmail.com>
2025-03-29 14:29:42 +0600

    effective way without allocation

--- src/2.add_two_numbers.cpp
+++ src/2.add_two_numbers.cpp
@@ -24,3 +24,41 @@ class Solution {
             return temp->next;
         }
 };
+
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *head = l1; 
+        ListNode *prev = nullptr;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr) {
+            int sum = carry;
+
+            if (l1 != nullptr) {
+                sum += l1->val;
+            }
+            if (l2 != nullptr) {
+                sum += l2->val;
+                if (l1 == nullptr) {
+                    prev->next = new ListNode(0);
+                    l1 = prev->next;
+                }
+            }
+
+            carry = sum / 10;
+            l1->val = sum % 10;
+
+            prev = l1;
+            l1 = l1->next;
+            if (l2 != nullptr) l2 = l2->next;
+        }
+
+        if (carry > 0) {
+            prev->next = new ListNode(carry);
+        }
+
+        return head;
+    }
+};
