1fd7191f657b36b5f45280ac15e5ccae7f4e9e06
knifemaster <emilasanbekov@gmail.com>
2025-03-30 12:32:50 +0600

    added optimal solution reverse_integer

--- src/7.reverse_integer.cpp
+++ src/7.reverse_integer.cpp
@@ -3,6 +3,30 @@
 #include <cstdlib>
 #include <cmath>
 
+
+#include <climits>
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+
+            // Проверка на переполнение перед умножением и сложением
+            if (rev > INT_MAX/10 || (rev == INT_MAX/10 && pop > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN/10 && pop < -8)) return 0;
+
+            rev = rev * 10 + pop;
+        }
+
+        return rev;
+    }
+};
+
+
 int reverse(int x) {
     int number;
     bool is_negative = false;
