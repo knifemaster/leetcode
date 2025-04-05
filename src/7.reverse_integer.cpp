a364081e2beca024b66bf17403f7bfad754de998
knifemaster <emilasanbekov@gmail.com>
2025-04-05 17:20:37 +0600

    fix

--- src/7.reverse_integer.cpp
+++ src/7.reverse_integer.cpp
@@ -10,14 +10,15 @@ class Solution {
 public:
     int reverse(int x) {
         int rev = 0;
-
+        constexpr const max = INT_MAX/10;
+       
         while (x != 0) {
             int pop = x % 10;
             x /= 10;
 
             // Проверка на переполнение перед умножением и сложением
-            if (rev > INT_MAX/10 || (rev == INT_MAX/10 && pop > 7)) return 0;
-            if (rev < INT_MIN/10 || (rev == INT_MIN/10 && pop < -8)) return 0;
+            if (rev > max || (rev == max && pop > 7)) return 0;
+            if (rev < max || (rev == max && pop < -8)) return 0;
 
             rev = rev * 10 + pop;
         }
---
+++ src/9.regular_expression_matching.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isMatch(string s, string p) {
+    
+   
+
+}
+
+
+int main() {
+
+    
+
+    return 0;
+}
