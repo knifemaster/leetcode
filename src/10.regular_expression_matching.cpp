a87a11474705b1cf432be0b76dafa3656b4fda94
knifemaster <emilasanbekov@gmail.com>
2025-04-05 22:50:52 +0600

    broken

--- src/10.regular_expression_matching.cpp
+++ src/10.regular_expression_matching.cpp
@@ -7,20 +7,33 @@
 using namespace std;
 
 
-
-
-int dfs(int i, int j) {
-    if (i == 0) {
-        return 0;
-    }
-    std::cout << i << " " <<j << std::endl;
-    return dfs(--i, --j);
-}
+// s = "aab";
+// p = ".*b"
+// dp = [false, false, false, false]
 
 
 bool isMatch(string s, string p) {
-    std::unordered_map<int, int> cache;
-    dfs(4, 3);    
+    size_t string_size = s.size();
+    size_t pattern_size = p.size();
+    std::vector<bool> dp(pattern_size + 1, false);
+
+    bool is_matched = false;
+
+    for (size_t i = 0; i < string_size; i++) {
+        
+        bool prev = dp[0];
+        dp[0] = i == 0;
+
+        for (size_t j = 0; j < pattern_size; j++) {
+            bool temp = dp[j];
+
+            if (p[j - 1] == '*') {
+                dp[j] = 
+            }
+
+        }
+    }
+
     return true;
 }
 
