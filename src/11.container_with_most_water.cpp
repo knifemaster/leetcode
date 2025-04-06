a3fae3776aa2292c5f850f2b90a5fc7934e3bffa
knifemaster <emilasanbekov@gmail.com>
2025-04-06 18:06:41 +0600

    added stub

---
+++ src/11.container_with_most_water.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int maxArea(vector<int>& height) {
+
+    return 49;
+}
+
+
+int main() {
+
+    vector<int> height = { 1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+    maxArea(height);
+
+
+    return 0
+}
