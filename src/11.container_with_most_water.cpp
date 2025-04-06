bcb8a63577249947130a31a2b469cc39770a1a78
knifemaster <emilasanbekov@gmail.com>
2025-04-06 18:53:41 +0600

    added optimal solution

--- src/11.container_with_most_water.cpp
+++ src/11.container_with_most_water.cpp
@@ -1,4 +1,31 @@
 #include <cmath>
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+    
+        int left = 0;
+        int right = height.size() - 1;
+        int max_area = 0;
+        int area;
+        while (left != right) {
+            area = std::min(height[left], height[right]) * (right - left);
+            max_area = std::max(area, max_area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+    return max_area;
+    }
+};
+
+
+
+#include <cmath>
 #include <iostream>
 #include <string>
 #include <vector>
