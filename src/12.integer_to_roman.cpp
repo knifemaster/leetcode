4c57acda7a0da049dffab211b03318c983acee72
knifemaster <emilasanbekov@gmail.com>
2025-04-06 21:17:48 +0600

    add dictionary to integerToRoman

--- src/12.integer_to_roman.cpp
+++ src/12.integer_to_roman.cpp
@@ -1,13 +1,43 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
+std::unordered_map<int, string> dictionary = {
+    { 1, "I" },
+    { 5, "V" },
+    { 10, "X" },
+    { 50, "L" },
+    { 100, "C" },
+    { 500, "D" },
+    { 1000, "M" }
+};
+
 string intToRoman(int num) {
+    
+    int thousands = num / 1000;
+    int hundreds = (num / 100) % 10;
+    int tens = (num/10) % 10;
+    int ones = num % 10;
+
+    std::cout << num << std::endl;
+    std::cout << thousands << std::endl;
+    std::cout << hundreds << std::endl;
+    std::cout << tens << std::endl;
+    std::cout << ones << std::endl;
+    
+    
+    //int thousands = num / 1000;
+    //int
+    
 
     return "";
 }
 
 int main() {
+
+    int number = 3749;
+    intToRoman(number);
 
 }
