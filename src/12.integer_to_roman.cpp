cc64fc3b10f9a40d5b7d978cd285e0e7da7595c6
knifemaster <emilasanbekov@gmail.com>
2025-04-06 23:39:29 +0600

    added const char* and reserve

--- src/12.integer_to_roman.cpp
+++ src/12.integer_to_roman.cpp
@@ -1,3 +1,30 @@
+    static const char* thousands[] = {"", "M", "MM", "MMM"};
+    static const char* hundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    static const char* tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    static const char* ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+class Solution {
+
+public:
+
+
+    string intToRoman(int num) {
+        
+        string result;
+        result.reserve(5);
+        result += thousands[num / 1000];
+        result += hundreds[(num % 1000) / 100];
+        result += tens[(num % 100) / 10];
+        result += ones[num % 10];
+        
+        return result;
+    }
+};
+
+
+
+
+
     static const string thousands[] = {"", "M", "MM", "MMM"};
     static const string hundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
     static const string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
