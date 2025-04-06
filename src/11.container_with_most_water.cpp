2e79c02e1229405c3b17201a3255fe99576eab8a
knifemaster <emilasanbekov@gmail.com>
2025-04-06 18:52:29 +0600

    added not optimal

--- src/11.container_with_most_water.cpp
+++ src/11.container_with_most_water.cpp
@@ -2,12 +2,41 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 int maxArea(vector<int>& height) {
+    for (const auto& h : height) {
+        std::cout << h << "  ";
+    }
+    std::cout << std::endl;
 
-    return 49;
+    int left = 0;
+    int right = height.size() - 1;
+
+    int max_area = 0;
+
+    while (left != right) {
+        int difference = std::abs(height[left] - height[right]);
+        int min_height = std::min(height[left], height[right]);
+        int square = min_height * min_height;
+        int width = right - left;
+
+        std::cout << square << " " << width << " " << min_height * width << std::endl;
+        int area = min_height* width;
+        max_area = std::max(area, max_area);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+
+    }
+
+    return max_area;
 }
 
 
@@ -15,8 +44,9 @@ int main() {
 
     vector<int> height = { 1, 8, 6, 2, 5, 4, 8, 3, 7};
 
-    maxArea(height);
+    std::cout << std::endl;
+    std::cout << maxArea(height);
 
 
-    return 0
+    return 0;
 }
