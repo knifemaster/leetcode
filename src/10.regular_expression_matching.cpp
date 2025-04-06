3abe4b24a9895f08ddd94b5838a8473e4a6f9804
knifemaster <emilasanbekov@gmail.com>
2025-04-06 17:29:54 +0600

    added solution with dp

--- src/10.regular_expression_matching.cpp
+++ src/10.regular_expression_matching.cpp
@@ -127,3 +127,48 @@ private:
         }
     }
 };
+
+
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <ranges>
+#include <vector>
+#include <utility>
+
+class Solution {
+public:
+
+    bool isMatch(string s, string p) {
+     int m = s.size(), n = p.size();
+    vector<bool> prev(n + 1, false);
+    vector<bool> curr(n + 1, false);
+    prev[0] = true;
+
+    for (int j = 1; j <= n; ++j) {
+        if (p[j - 1] == '*') {
+            prev[j] = prev[j - 2];
+        }
+    }
+
+    for (int i = 1; i <= m; ++i) {
+        curr[0] = false;
+        for (int j = 1; j <= n; ++j) {
+            if (p[j - 1] == '*') {
+                curr[j] = curr[j - 2];
+                if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
+                    curr[j] = curr[j] || prev[j];
+                }
+            } else if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
+                curr[j] = prev[j - 1];
+            } else {
+                curr[j] = false;
+            }
+        }
+        prev = curr;
+    }
+
+    return prev[n];
+    }
+};
