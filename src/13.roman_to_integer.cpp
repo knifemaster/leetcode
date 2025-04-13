059477c51b6b9b09ce7f3610fa7dbcf3dc71bfb8
knifemaster <emilasanbekov@gmail.com>
2025-04-13 12:33:31 +0600

    added longest_common_prefix

--- src/13.roman_to_integer.cpp
+++ src/13.roman_to_integer.cpp
@@ -1,14 +1,50 @@
 #include <iostream>
 #include <unordered_map>
+#include <string>
 
+int romanToInt(std::string si) {
 
-int romanToInt(string s) {
-    std::unordered_map<std::string, int> dictionary = {};
-    return 0;
+ 
+    std::unordered_map<std::string, int> dict {
+            {"M_", 1000 },
+            {"D_", 500},
+            {"CM", -100},
+            {"CD", -100},
+            {"C_", 100},
+            {"L_", 50},
+            {"XC", -10},
+            {"XL", -10},
+            {"X_", 10},
+            {"V_", 5},
+            {"IX", -1},
+            {"IV", -1},
+            {"I_", 1}
+        };
+
+        int result = 0;
+        bool b = si.size() % 2 == 0;
+        //si.resize(si.size() + 1);
+        if (!b) {
+            si += "_";
+        }
+        std::cout << si << std::endl;
+        for (int i = 1; i < si.size(); i += 2) {
+           // std::cout << si[i - 1] << si[i];
+            std::cout << si[i-1] << si[i] << std::endl;
+            std::string characters;
+            characters += si[i-1]; //+ si[i];
+            characters += si[i];
+            std::cout << characters << std::endl;
+            result += dict[characters];
+        }
+
+//    std::cout << si << std::endl;    
+    return result;
 }
 
 int main() {
-    std::string roman = "XIV";
+    std::string roman = "XCL";
+    std::cout << romanToInt(roman);
     return 0;
 }
 
---
+++ src/14.longest_common_prefix.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <unordered_set>
+
+
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    int index = 0;
+    char character;
+    string result;
+    bool is_difference = true;
+    //std::cout << strs[0][0];
+    std::string word;
+    while (true) {
+        word = strs[0];
+        character = word[index];
+        for (const auto& word : strs) {
+            if (word[index] == character) {
+                continue;
+            } else {
+                is_difference = false;
+                break;
+            }
+        }
+        if(!is_difference) {
+            break;
+        }
+        result += character;
+        index++;
+    }
+
+
+    return result;
+}
+
+
+int main() {
+    vector<string> vecs = {"flower", "flow", "flight"};
+    vector<string> vec_one_word = {"a"};
+    vector<string> vec_flowers = {"flower", "flower", "flower"};
+    cout << longestCommonPrefix(vec_flowers)<< "b" << endl; 
+
+
+}
