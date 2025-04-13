fe9b1a3da943aca23ddc5bf9526a3dbcead262f3
knifemaster <emilasanbekov@gmail.com>
2025-04-13 18:45:24 +0600

    added solution with optimization

--- src/16.3sum_closest.cpp
+++ src/16.3sum_closest.cpp
@@ -9,6 +9,74 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
         const int n = nums.size();
+        int closest = nums[0] + nums[1] + nums[n-1]; // Лучшая начальная инициализация
+        
+        for (int i = 0; i < n - 2; ) {
+            const int a = nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            
+            // Минимально возможная сумма для текущего i
+            const int min_sum = a + nums[left] + nums[left+1];
+            if (min_sum > target) {
+                if (abs(min_sum - target) < abs(closest - target)) {
+                    closest = min_sum;
+                }
+                break;
+            }
+            
+            // Максимально возможная сумма для текущего i
+            const int max_sum = a + nums[right-1] + nums[right];
+            if (max_sum < target) {
+                if (abs(max_sum - target) < abs(closest - target)) {
+                    closest = max_sum;
+                }
+                while (i < n-2 && nums[i] == a) i++;
+                continue;
+            }
+            
+            while (left < right) {
+                const int b = nums[left];
+                const int c = nums[right];
+                const int sum = a + b + c;
+                
+                if (sum == target) {
+                    return target;
+                }
+                
+                if (abs(sum - target) < abs(closest - target)) {
+                    closest = sum;
+                }
+                
+                if (sum < target) {
+                    while (left < right && nums[left] == b) left++;
+                } else {
+                    while (left < right && nums[right] == c) right--;
+                }
+            }
+            
+            while (i < n-2 && nums[i] == a) i++;
+        }
+        return closest;
+    }
+};
+
+
+
+
+
+
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        const int n = nums.size();
         int closest = nums[0] + nums[1] + nums[2]; // Начальное значение
         
         for (int i = 0; i < n - 2; ++i) {
