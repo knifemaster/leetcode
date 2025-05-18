991e52cbfd4484c7f0a4dd6043405ac1631dbb8a
knifemaster <emilasanbekov@gmail.com>
2025-05-18 13:14:13 +0600

    added main and struct ListNode

---
+++ src/19.remove_nth_node_from_end_of_list.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <print>
+#include <vector>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+}
+
+
+int main() {
+
+    return 0;
+}
