307a7f21a100b54d38b190acbbb40cd2828b1346
knifemaster <emilasanbekov@gmail.com>
2025-05-18 13:07:19 +0600

    added letter combinations solution

--- src/17.letter_combinations_of_a_phone_number.cpp
+++ src/17.letter_combinations_of_a_phone_number.cpp
@@ -1,15 +1,14 @@
+#include <memory_resource>
 #include <ranges>
-#include <vector>
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <unordered_map>
 #include <print>
 
+namespace rv = std::ranges::views;
 using namespace std;
 
 
-vector<string> letterCombinations(string digits) {
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
     vector<string> buttons = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"} ;
     vector<string> combinations;
     unordered_map<int, string> combinationes;
@@ -22,24 +21,29 @@ vector<string> letterCombinations(string digits) {
 
     vector<int> button_numbers;
 
+    if (length == 1) {
+        auto index = digits[0] - '0';
+        for (auto letter : buttons[index]) {
+            std::string s;
+            s.push_back(letter);
+            combinations.push_back(s);
+        }
+    }
+
     for (auto& digit : digits) {
         auto digit_int = digit - '0';
         button_numbers.push_back(digit_int);
-        std::print("{}", digit_int);
     }
 
     if (length == 2) {
         for (auto&& [a, b] : rv::cartesian_product(buttons[button_numbers[0]], buttons[button_numbers[1]])) {
             std::string s;
-            std::print(" {}{}", a, b);
             s.push_back(a);
             s.push_back(b);
-
             combinations.push_back(s);
 
         }
     }
-
     if (length == 3) {
         for (auto&& [a, b, c] : rv::cartesian_product(buttons[button_numbers[0]], buttons[button_numbers[1]], buttons[button_numbers[2]])) {
             std::string s;
@@ -48,23 +52,24 @@ vector<string> letterCombinations(string digits) {
             s.push_back(c);
 
             combinations.push_back(s);
-            std::print("{} {} {}", a, b, c);
         }
     }
 
-    std::print("{}", combinations);
+    if (length == 4) {
+        for (auto&& [a, b, c, d] : rv::cartesian_product(buttons[button_numbers[0]], buttons[button_numbers[1]], buttons[button_numbers[2]], buttons[button_numbers[3]])) {
+            std::string s;
+            s.push_back(a);
+            s.push_back(b);
+            s.push_back(c);
+            s.push_back(d);
+
+            combinations.push_back(s);
+        }
+
+    }
     return combinations;        
 
 }
-
-
-int main() {
-    string digits = "23";
-
-    auto combinations = letterCombinations(digits);
+   
     
-
-
-    return 0;
-
-}
+};
