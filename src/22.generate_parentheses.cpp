b9b20636deeea96a42089676e9775ea37ef88a68
knifemaster <emilasanbekov@gmail.com>
2025-05-28 12:21:59 +0600

    added main function

--- src/22.generate_parentheses.cpp
+++ src/22.generate_parentheses.cpp
@@ -5,12 +5,37 @@
 
 
 std::vector<std::string> generateParenthesis(int n) {
+    std::vector<std::string> result;
+    std::string left_parens = "()()()";
+    std::string right_parens = "()()()";
+    std::string mid_parens = "()()()";
+    std::string last_parens = "()()()";
+
+    for (const auto& [a, b, c, d] : std::ranges::views::cartesian_product(left_parens, right_parens, mid_parens, last_parens)) {
+        std::cout << a << " " << b << " " << c  << " "<< d << std::endl;
+    }
+
+    return result;
+
+}
+
+std::string parentheses = "()()()()";
+int size = parentheses.size();
+
+
+std::string backtrack(int n) {
+    if (n > size) {
+        return ""; 
+    }
+
+    return backtrack(n--);
 
 }
 
 
+
 int main() {
-
-
+    generateParenthesis(4);
+    
     return 0;
 }
