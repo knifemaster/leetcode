8158f0898ad4984213922c81b1a7409bbadc34b6
knifemaster <emilasanbekov@gmail.com>
2025-06-03 19:44:45 +0600

    added solution

--- src/22.generate_parentheses.cpp
+++ src/22.generate_parentheses.cpp
@@ -1,54 +1,3 @@
-#include <iostream>
-#include <string>
-#include <ranges>
-#include <vector>
-#include <stack>
-
-
-std::string is_valid(std::string& s) {
-    std::stack<char> queue;
-    int left_paren_count = 0;
-    int right_paren_count = 0;
-    
-    if (s.size() % 2 != 0) {
-        return "";
-    }
-    bool is_occur = false;
-    for (const auto& parentheses : s) {
-        if (parentheses == '(') {    
-            queue.push(parentheses);
-            left_paren_count++;
-        } else {
-            right_paren_count++;
-            is_occur = true;
-            
-            if (queue.empty()) {
-                return "";
-            }
-            char close_paren = queue.top();
-            if (parentheses == ')') {
-                if (close_paren == '(') {
-                    queue.pop();
-                } else {
-                    return "";
-                }
-            }
-        }
-
-
-    }
-
-    if (left_paren_count != right_paren_count) {
-        return "";
-    }
-
-    if (is_occur) 
-        return s;
-    return "";
-    }
-    
-
-
 
 static std::vector<std::string> parentheses = {
             "",
@@ -62,63 +11,85 @@ static std::vector<std::string> parentheses = {
             "()()()()()()()()"
 };
 
+class Solution {
+public:
+
+    std::string is_valid(std::string& s) {
+        std::stack<char> queue;
+        int left_paren_count = 0;
+        int right_paren_count = 0;
+        
+        if (s.size() % 2 != 0) {
+            return "";
+        }
+        bool is_occur = false;
+        for (const auto& parentheses : s) {
+            if (parentheses == '(') {    
+                queue.push(parentheses);
+                left_paren_count++;
+            } else {
+                right_paren_count++;
+                is_occur = true;
+                
+                if (queue.empty()) {
+                    return "";
+                }
+                char close_paren = queue.top();
+                if (parentheses == ')') {
+                    if (close_paren == '(') {
+                        queue.pop();
+                    } else {
+                        return "";
+                    }
+                }
+            }
 
 
-std::vector<std::string> generateParenthesis(int n) {
+    }
 
-    std::vector<std::string> result;
-    std::string temp = parentheses[n];
+    if (left_paren_count != right_paren_count) {
+        return "";
+    }
 
-    std::prev_permutation(temp.begin(), temp.end());
+    if (is_occur) 
+        return s;
+    return "";
+    }
 
+    vector<string> generateParenthesis(int n) {
+
+        if (n == 1) {
+            return {"()"};
+
+        }
     
-    do {
-        std::cout << temp << '\n';
-        if (is_valid(temp) != "") {
-            result.push_back(temp);
-        }
-    }
+        std::vector<std::string> result;
+        result.reserve(n*n*3);
+        std::string temp = parentheses[n];
 
-    while (std::prev_permutation(temp.begin(), temp.end()));
-
-    std::string second_part = parentheses[n];
-
-
-    do {
-        if (is_valid(second_part) != "") {
-            std::cout << second_part << '\n';
-            result.push_back(second_part);
-        }
-    }
-    while (std::next_permutation(second_part.begin(), second_part.end()));
-
-
+        std::prev_permutation(temp.begin(), temp.end());
 
         
-    return result;
-
-}
-
-
-int main() {
-    //for (int i = 0; i < 8; i++) {
-    //generateParenthesis(i);
-    //}
-    
-
-    std::vector<std::string> vec_parens = generateParenthesis(4);
-
-    std::cout << "*******************************" << std::endl;
-    for (std::string& paren : vec_parens) {
-    
-        std::string valid = is_valid(paren);
-   //     std::cout << valid << std::endl;
-        if (valid != "") {
-            std::cout << valid << std::endl;
+        do {
+            //std::cout << temp << '\n';
+            if (is_valid(temp) != "") {
+                result.push_back(temp);
+            }
         }
+
+        while (std::prev_permutation(temp.begin(), temp.end()));
+
+        std::string second_part = parentheses[n];
+
+
+        do {
+            if (is_valid(second_part) != "") {
+                //std::cout << second_part << '\n';
+                result.push_back(second_part);
+            }
+        }
+        while (std::next_permutation(second_part.begin(), second_part.end()));
+        
+        return result;
     }
-
-
-
-    return 0;
-}
+};
