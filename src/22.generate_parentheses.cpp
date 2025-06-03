9595f8bab5863619be383d4070afedd2d0985509
knifemaster <emilasanbekov@gmail.com>
2025-06-03 19:39:38 +0600

    added solution of generate parentheses

--- src/22.generate_parentheses.cpp
+++ src/22.generate_parentheses.cpp
@@ -2,40 +2,123 @@
 #include <string>
 #include <ranges>
 #include <vector>
+#include <stack>
+
+
+std::string is_valid(std::string& s) {
+    std::stack<char> queue;
+    int left_paren_count = 0;
+    int right_paren_count = 0;
+    
+    if (s.size() % 2 != 0) {
+        return "";
+    }
+    bool is_occur = false;
+    for (const auto& parentheses : s) {
+        if (parentheses == '(') {    
+            queue.push(parentheses);
+            left_paren_count++;
+        } else {
+            right_paren_count++;
+            is_occur = true;
+            
+            if (queue.empty()) {
+                return "";
+            }
+            char close_paren = queue.top();
+            if (parentheses == ')') {
+                if (close_paren == '(') {
+                    queue.pop();
+                } else {
+                    return "";
+                }
+            }
+        }
+
+
+    }
+
+    if (left_paren_count != right_paren_count) {
+        return "";
+    }
+
+    if (is_occur) 
+        return s;
+    return "";
+    }
+    
+
+
+
+static std::vector<std::string> parentheses = {
+            "",
+            "()",
+            "()()",
+            "()()()",
+            "()()()()",
+            "()()()()()",
+            "()()()()()()",
+            "()()()()()()()",
+            "()()()()()()()()"
+};
+
 
 
 std::vector<std::string> generateParenthesis(int n) {
-    std::vector<std::string> result;
-    std::string left_parens = "()()()";
-    std::string right_parens = "()()()";
-    std::string mid_parens = "()()()";
-    std::string last_parens = "()()()";
 
-    for (const auto& [a, b, c, d] : std::ranges::views::cartesian_product(left_parens, right_parens, mid_parens, last_parens)) {
-        std::cout << a << " " << b << " " << c  << " "<< d << std::endl;
+    std::vector<std::string> result;
+    std::string temp = parentheses[n];
+
+    std::prev_permutation(temp.begin(), temp.end());
+
+    
+    do {
+        std::cout << temp << '\n';
+        if (is_valid(temp) != "") {
+            result.push_back(temp);
+        }
     }
 
+    while (std::prev_permutation(temp.begin(), temp.end()));
+
+    std::string second_part = parentheses[n];
+
+
+    do {
+        if (is_valid(second_part) != "") {
+            std::cout << second_part << '\n';
+            result.push_back(second_part);
+        }
+    }
+    while (std::next_permutation(second_part.begin(), second_part.end()));
+
+
+
+        
     return result;
 
 }
 
-std::string parentheses = "()()()()";
-int size = parentheses.size();
-
-
-std::string backtrack(int n) {
-    if (n > size) {
-        return ""; 
-    }
-
-    return backtrack(n--);
-
-}
-
-
 
 int main() {
-    generateParenthesis(4);
+    //for (int i = 0; i < 8; i++) {
+    //generateParenthesis(i);
+    //}
     
+
+    std::vector<std::string> vec_parens = generateParenthesis(4);
+
+    std::cout << "*******************************" << std::endl;
+    for (std::string& paren : vec_parens) {
+    
+        std::string valid = is_valid(paren);
+   //     std::cout << valid << std::endl;
+        if (valid != "") {
+            std::cout << valid << std::endl;
+        }
+    }
+
+
+
     return 0;
 }
