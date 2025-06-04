117251aa95975d91aee692785a334843656b5dc6
knifemaster <emilasanbekov@gmail.com>
2025-06-04 16:38:43 +0600

    printin lists

--- src/23.merge_k_sorted_lists.cpp
+++ src/23.merge_k_sorted_lists.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
-#include <priority_queue>
+#include <queue>
 
 
 struct ListNode {
@@ -13,14 +13,6 @@ struct ListNode {
 
 };
 
-
-ListNode* mergeKLists(std::vector<ListNode*>& lists) {
-    ListNode* list = new ListNode(1);
-
-
-    return list
-
-}
 
 void printListNode(ListNode* root) {
     
@@ -34,6 +26,21 @@ void printListNode(ListNode* root) {
 
 }
 
+
+ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+    ListNode* result = new ListNode(1);
+
+    for (auto list : lists) {
+        printListNode(list);
+        std::cout << std::endl;
+    }
+
+    return result;
+
+}
+
+
+
 int main() {
     ListNode* list1 = new ListNode(1, new ListNode(4, new ListNode(5)));
     ListNode* list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
@@ -44,7 +51,7 @@ int main() {
     vec_lists.push_back(list2);
     vec_lists.push_back(list3);
 
-    mergeKlists(vec_lists);
+    mergeKLists(vec_lists);
 
 
 
