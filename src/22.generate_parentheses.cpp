8c77800fe814afc9b78a907117a0277d594d3934
knifemaster <emilasanbekov@gmail.com>
2025-06-04 16:00:47 +0600

    added solution

--- src/22.generate_parentheses.cpp
+++ src/22.generate_parentheses.cpp
@@ -93,3 +93,32 @@ public:
         return result;
     }
 };
+
+
+
+
+
+class Solution {
+public:
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    void backtrack(vector<string>& res, string current, int open, int close, int max) {
+        if (current.length() == max * 2) {
+            res.push_back(current);
+            return;
+        }
+
+        if (open < max) {
+            backtrack(res, current + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(res, current + ")", open, close + 1, max);
+        }
+    }
+};
+
