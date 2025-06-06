da0b57843422be10ca7606d042cded02353e5641
knifemaster <emilasanbekov@gmail.com>
2025-06-06 18:51:30 +0600

    added solution with vector

--- src/23.merge_k_sorted_lists.cpp
+++ src/23.merge_k_sorted_lists.cpp
@@ -1,88 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <print>
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void visitList(ListNode* root, std::vector<int>& vec) {
+        if (!root) return;
+        vec.push_back(root->val);
+        visitList(root->next, vec); 
+    }
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+        std::vector<int> res;
+        
+        if (lists.empty()) {
+            return nullptr;
+        }
 
+        for (auto list : lists) {
+            visitList(list, res);
+        }
+
+        if (res.empty()) {
+            return nullptr;
+        }
+
+        std::sort(res.begin(), res.end());
+
+        ListNode* result = new ListNode(res[0]);
+        ListNode* current = result;
+
+        for (size_t i = 1; i < res.size(); ++i) {
+            current->next = new ListNode(res[i]);
+            current = current->next;
+        }
+
+        return result;
+    }
 };
-
-
-void printListNode(ListNode* root) {
-    
-    if (root) {
-        std::cout << root->val << " ";
-    } else {
-        return;
-    }
-
-    printListNode(root->next);
-
-}
-
-
-std::priority_queue<int, std::vector<int>, std::greater<int>> heap;
-i
-void visitList(ListNode* root) {
-    if (root) {
-        heap.push(root->val);
-    } else {
-        return;
-    }
-
-    visitList(root->next);
-
-}
-
-ListNode* mergeKLists(std::vector<ListNode*>& lists) {
-    ListNode* result = new ListNode(1);
-
-    std::vector<int> values;
-    values.reserve(lists.size());
-    
-    //std::priority_queue<int> heap;
-    
-    for (auto list : lists) {
-        //printListNode(list);
-        visitList(list);
-    }
-        std::cout << std::endl;
-
-
-
-    return result;
-
-}
-
-
-
-int main() {
-    ListNode* list1 = new ListNode(1, new ListNode(4, new ListNode(5)));
-    ListNode* list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-    ListNode* list3 = new ListNode(2, new ListNode(6));
-
-    std::vector<ListNode*> vec_lists;
-    vec_lists.push_back(list1);
-    vec_lists.push_back(list2);
-    vec_lists.push_back(list3);
-
-    mergeKLists(vec_lists);
-
-    ListNode* res;
-
-    //std::print("{}", heap);
-    std::cout << std::endl;
-    while (!heap.empty()) {
-        std::cout << heap.top() << " ";
-       // res->next = new ListNode(heap.top());
-        heap.pop();
-    }
-    return 0;
-}
-
