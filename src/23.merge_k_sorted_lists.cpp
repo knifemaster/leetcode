8e09b36d3eb1e4dd2f6bad78d3a15b668b5d58fc
knifemaster <emilasanbekov@gmail.com>
2025-06-06 18:54:20 +0600

    added solution with priority_queue

--- src/23.merge_k_sorted_lists.cpp
+++ src/23.merge_k_sorted_lists.cpp
@@ -44,3 +44,73 @@ public:
         return result;
     }
 };
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <print>
+
+class Solution {
+public:
+    //std::priority_queue<int, std::vector<int>, std::greater<int>> heap;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> heap;
+    void visitList(ListNode* root) {
+        if (root) {
+            heap.push(root->val);
+        } else {
+            return;
+        }
+
+        visitList(root->next);
+
+    }
+
+    ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+
+    
+        if (lists.empty()) {
+            return nullptr;
+        }
+
+        for (auto list : lists) {
+            visitList(list);
+        }
+
+        ListNode* result;
+        ListNode* current;
+        
+        if (!heap.empty()) {
+            result = new ListNode(heap.top());
+            current = result;
+            heap.pop();
+        } else {
+            return nullptr;
+        }
+
+        while (!heap.empty()) {
+                std::cout << heap.top() << " ";
+                current->next = new ListNode(heap.top());
+                current = current->next;
+                heap.pop();
+        }
+
+        return result;
+    }
+
+
+
+};
