274ed34e589dd47a5b0a44fd61875f52b04621bf
knifemaster <emilasanbekov@gmail.com>
2025-06-06 18:55:48 +0600

    added fixed solution

--- src/21.merge_two_sorted_lists.cpp
+++ src/21.merge_two_sorted_lists.cpp
@@ -8,59 +8,25 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <print>
-
 class Solution {
 public:
-    std::priority_queue<int, std::vector<int>, std::greater<int>> heap;
-
-    void visitList(ListNode* root) {
-        if (root) {
-            heap.push(root->val);
-        } else {
-            return;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    
+        if (list1 == nullptr) {
+            return list2;
         }
 
-        visitList(root->next);
+        if (list2 == nullptr) {
+            return list1;
+        }
+
+        if (list1->val <= list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
 
     }
-
-    ListNode* mergeKLists(std::vector<ListNode*>& lists) {
-
-        if (lists.empty()) {
-            return nullptr;
-        }
-
-        for (auto list : lists) {
-            visitList(list);
-        }
-
-        ListNode* result;
-        ListNode* current;
-        
-        if (!heap.empty()) {
-            result = new ListNode(heap.top());
-            current = result;
-            heap.pop();
-        } else {
-            return nullptr;
-        }
-
-        while (!heap.empty()) {
-                std::cout << heap.top() << " ";
-                current->next = new ListNode(heap.top());
-                current = current->next;
-                heap.pop();
-        }
-
-        return result;
-    }
-
-
-
 };
