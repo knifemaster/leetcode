db5f13eda69d9336da3633b5515a3ee05162e11b
knifemaster <emilasanbekov@gmail.com>
2025-06-07 16:18:39 +0600

    added stub

---
+++ src/25.reverse_nodes_in_k_group.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <stack>
+#include <iostream>
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+}
+
+
+ListNode* reverseKGroup(ListNode* head, int k) {
+
+}
+
+int main() {
+
+
+
+    return 0;
+
+}
