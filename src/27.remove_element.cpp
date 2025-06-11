c69fcd4cb9343097d3f09635c01fa9d422ab60a9
knifemaster <emilasanbekov@gmail.com>
2025-06-12 00:56:32 +0600

    refactor

--- src/27.remove_element.cpp
+++ src/27.remove_element.cpp
@@ -6,11 +6,11 @@
 int removeElement(std::vector<int>& nums, int val) {
 
     int left = 0;
-    int right = nums.size();
+    int right = nums.size() - 1;
 
     while (left < right) {
         if (nums[left] == val) {
-            nums[left] = nums[right - 1];
+            nums[left] = nums[right];
             right--;
         } else {
             left++;
