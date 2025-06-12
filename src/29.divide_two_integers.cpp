4238c84add18766166edd713aef88b99735c00bc
knifemaster <emilasanbekov@gmail.com>
2025-06-12 18:56:07 +0600

    added not full solution

---
+++ src/29.divide_two_integers.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+#include <limits>
+
+int divide(int dividend, int divisor) {
+
+    bool is_negative;
+
+    if (((dividend > 0) && (divisor > 0)) || ((dividend < 0) && (divisor < 0))) {
+        is_negative = false;
+    } else {
+        is_negative = true;
+    }
+
+    if (dividend == std::numeric_limits<int>::max() && divisor == -1) {
+        return std::numeric_limits<int>::max();
+    }
+
+    long long dividend_abs = abs(static_cast<long long>(dividend));
+    long long divisor_abs = abs(static_cast<long long>(divisor));
+
+    long long result = 0;
+
+    for (int exponent = 31; exponent >= 0; exponent--) {
+        if ((divisor_abs << exponent) <= dividend_abs) {
+            dividend -= (divisor_abs << exponent);
+            result += (1LL << exponent);
+            std::cout << "dividend : " << dividend << std::endl;
+            std::cout << "result : " << result << std::endl;
+        }       
+
+    }
+
+    result = is_negative ? -result : result;
+
+
+    if (result < std::numeric_limits<int>::min()) {
+        return std::numeric_limits<int>::min();
+    }
+
+    if (result > std::numeric_limits<int>::max()) {
+        return std::numeric_limits<int>::max();
+    }
+    
+
+    //if (is_negative) {
+    //    return -result;
+    //} else {
+    //    return result;
+    //}
+
+
+    return result;
+
+}
+
+
+int main() {
+    int dividend = 7;
+    int divisor = -3;
+
+    std::cout << divide(dividend, divisor);
+
+
+    return 0;
+}
