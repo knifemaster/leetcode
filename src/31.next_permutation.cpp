3e161d072564be884525dba0dd85049a38bb469f
knifemaster <emilasanbekov@gmail.com>
2025-06-17 12:10:58 +0600

    added stub

---
+++ src/31.next_permutation.cpp
@@ -0,0 +1,18 @@
+#include <ranges>
+#include <algorithm>
+#include <iostream>
+#include <print>
+#include <vector>
+
+void nextPermutation(std::vector<int>& nums) {
+
+    return;
+}
+
+
+int main() {
+    
+    return 0;
+}
+
+
