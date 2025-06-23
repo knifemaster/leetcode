289144eb0cf341d91ae3ee8696a529e2c53688f0
knifemaster <emilasanbekov@gmail.com>
2025-06-24 01:42:14 +0600

    added solution with equal_range

--- src/34.find_first_and_last_position_of_element_in_sorted_array.cpp
+++ src/34.find_first_and_last_position_of_element_in_sorted_array.cpp
@@ -1,15 +1,35 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <print>
 
 
-std::vector<int> searchRange(std::vector<int>& nums, int target) {
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+    const auto range = std::equal_range(nums.begin(), nums.end(), target);
+    std::vector<int> result;
+    result.reserve(2);
 
+    if (std::distance(range.first, range.second) == 0) {
+        return {-1, -1};
+    }
 
-}
+    result.push_back(std::distance(nums.begin(), range.first));
+    result.push_back(std::distance(nums.begin(), range.second) - 1);
+
+    return result;    
+    }
+};
+
 
 int main() {
 
+    std::vector<int> numbers = { 5, 7, 7, 8,  8, 10 };
+
+    int target = 6;
+
+    std::print("{}", searchRange(numbers, target));
 
 
     return 0;
