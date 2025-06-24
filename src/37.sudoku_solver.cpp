653786866b1deaa5ea782ea2e92d6652793ca66d
knifemaster <emilasanbekov@gmail.com>
2025-06-24 23:01:25 +0600

    added stub

---
+++ src/37.sudoku_solver.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+
+
+
+using namespace std;
+
+
+bool is_possible_moves(vector<vector<char>>& board, unordered_set<char>& row_state, unordered_set<char>& column_state, unordered_set<char>& sub_box) {
+
+
+    return true;
+    // need to return possible numbers
+}
+
+
+
+void solveSudoku(std::vector<std::vector<char>>& board) {
+
+
+    return;
+
+}
+
+
+int main() {
+
+
+
+    return 0;
+
+}
