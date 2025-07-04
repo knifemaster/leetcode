9dd0d6f3b3d9af3ea3a2ff38ae1a650a44b851c4
knifemaster <emilasanbekov@gmail.com>
2025-07-04 23:56:06 +0600

    added stub for solution

---
+++ src/38.count_and_say.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <map>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+
+std::string countAndSay(int n) {
+
+
+    return "";
+}
+
+int main() {
+
+
+
+    return 0;
+}
