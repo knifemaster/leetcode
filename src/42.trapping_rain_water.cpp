590db3493e707f58ce3ef990a92287e2c27d487d
knifemaster <emilasanbekov@gmail.com>
2025-07-07 23:08:20 +0600

    added solution trapping raing water

--- src/42.trapping_rain_water.cpp
+++ src/42.trapping_rain_water.cpp
@@ -7,7 +7,36 @@
 
 
 int trap(std::vector<int>& height) {
-    
+
+    std::stack<int> temp;
+    int volume = 0;
+
+    for (int index = 0; index < height.size(); ++index) {
+        
+        while(!temp.empty() && height[index] > height[temp.top()]) {
+            int top = temp.top();
+            temp.pop();
+
+            if (temp.empty()) {
+                break;
+            }
+
+            int distance = index - temp.top() - 1;
+            int aligned_height = std::min(height[index], height[temp.top()]) - height[top];        
+            std::cout << "distance: " << distance << std::endl;
+            std::cout << "boundedHeight: " << aligned_height << std::endl;
+
+            volume += distance * aligned_height;
+            std::cout << "volume: " << volume << std::endl;
+
+        }        
+
+        temp.push(index);
+
+    }
+
+    return volume;
+
 }
 
 
