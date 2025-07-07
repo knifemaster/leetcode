1c0ada91528a58788d3d5de87c086dc33b202bd5
knifemaster <emilasanbekov@gmail.com>
2025-07-07 22:14:00 +0600

    added stub for trapping rain water

---
+++ src/42.trapping_rain_water.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <print>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+
+
+int trap(std::vector<int>& height) {
+    
+}
+
+
+int main() {
+
+    std::vector<int> height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+
+    std::cout << "trapped water: " << trap(height) << std::endl;
+
+    return 0;
+
+}
