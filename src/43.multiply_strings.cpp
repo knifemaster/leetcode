795f57e2f9fd4ba23713ef0d2935fcec725305d2
knifemaster <emilasanbekov@gmail.com>
2025-07-07 23:20:12 +0600

    added stub for multiply string

---
+++ src/43.multiply_strings.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+std::string multiply(std::string& num1, std::string& num2) {
+
+    return "";
+}
+
+
+int main() {
+
+
+
+
+
+    return 0;
+}
