674841a35932590c8f2908e3bf393c5773206f36
knifemaster <emilasanbekov@gmail.com>
2025-07-09 00:16:12 +0600

    added stub jumg game2

---
+++ src/45.jump_game_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+
+
+int jump(std::vector<int>& nums) {
+
+    vector<int> dp;
+    dp.reserve(nums.size());
+
+
+    for (int i = 0; i < nums.size(); i++) {
+    
+
+
+    }
+
+    return 0;
+}
+
+
+int main() {
+
+
+    return 0;
+}
