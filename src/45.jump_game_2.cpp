44282865b4f9f20f371f56dd5864a01b525170e6
knifemaster <emilasanbekov@gmail.com>
2025-07-09 19:28:57 +0600

    added jump game II solution

--- src/45.jump_game_2.cpp
+++ src/45.jump_game_2.cpp
@@ -1,28 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+#include <print>
 
 
 
 int jump(std::vector<int>& nums) {
 
-    vector<int> dp;
-    dp.reserve(nums.size());
+    int jump_count = 0;
+    int left = 0; 
+    int right = 0;
 
+    int last_index = nums.size() - 1;
 
-    for (int i = 0; i < nums.size(); i++) {
-    
+    while (right < last_index) {
+        int farthest = 0;
 
+        std::cout << "left: " << left << std::endl;
+        std::cout << "right: " << right << std::endl;
+        
+
+        for (int i = left; i < right + 1; i++) {
+            std::cout << "farthest: " << farthest << " i: " << i << " i+nums[i]: " << i + nums[i] << std::endl;
+            farthest = std::max(farthest, i + nums[i]);
+        }
+
+        std::cout << "jump count" << jump_count << std::endl;
+
+        left = right + 1;
+        right = farthest;
+        jump_count += 1;
 
     }
 
-    return 0;
+    return jump_count;
 }
 
 
 int main() {
 
+    std::vector<int> nums = {2, 3, 1, 1, 4};
+
+    std::cout << jump(nums) << std::endl;
 
     return 0;
 }
